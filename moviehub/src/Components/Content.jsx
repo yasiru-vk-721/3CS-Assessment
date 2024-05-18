ef9ce043dee4dddb6274b73b459a1d08c14883fd
YasiruVK <145313332+yasiru-vk@users.noreply.github.com>
2024-05-18 13:05:27 +0530

    created slideshow and footer

--- moviehub/src/Components/Content.jsx
+++ moviehub/src/Components/Content.jsx
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { motion } from 'framer-motion';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+// Import local images
+import image1 from '../assets/image1.jpeg';
+import image2 from '../assets/image2.jpeg';
+import image3 from '../assets/image3.jpeg';
+import image4 from '../assets/image4.jpeg';
+import image5 from '../assets/image5.jpeg';
+import image6 from '../assets/image6.jpeg';
+
+// Import Footer component
+import Footer from './Footer';
 
 const Content = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [showDetails, setShowDetails] = useState(false);
-  const [slideshowMovies, setSlideshowMovies] = useState([]);
+  const [additionalMovies, setAdditionalMovies] = useState([]);
 
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
@@ -39,13 +53,17 @@ const Content = () => {
     }
   };
 
-  const fetchSlideshowMovies = async () => {
+  const fetchAdditionalMovies = async () => {
     try {
-      const response = await axios.get('NEW_API_URL'); // Replace NEW_API_URL with the URL of the new API
-      setSlideshowMovies(response.data.results.slice(0, 10)); // Assuming the API returns an array of movies
+      const response = await axios.get(`https://www.omdbapi.com/?s=comedy&apikey=f8e2f17b`);
+      if (response.data.Response === 'True') {
+        setAdditionalMovies(response.data.Search.slice(0, 7));
+      } else {
+        setAdditionalMovies([]);
+      }
     } catch (error) {
-      console.error('Error fetching slideshow movies:', error);
-      setSlideshowMovies([]);
+      console.error('Error fetching additional movies:', error);
+      setAdditionalMovies([]);
     }
   };
 
@@ -63,9 +81,9 @@ const Content = () => {
   };
 
   useEffect(() => {
-    const defaultQuery = 'warrior'; 
+    const defaultQuery = `kid`; 
     fetchMovies(defaultQuery);
-    fetchSlideshowMovies(); // Fetch slideshow movies when component mounts
+    fetchAdditionalMovies(); // Fetch additional movies when component mounts
   }, []);
 
   useEffect(() => {
@@ -74,58 +92,110 @@ const Content = () => {
     }
   }, [searchQuery]);
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true, // Add fade effect
+    adaptiveHeight: true
+  };
+
+  // List of local images
+  const slideshowImages = [image1, image2, image3, image4, image5, image6]; // Use unique images when uncommenting more
+
   return (
-    <div>
-      {/* Slideshow component */}
-      <div className="slideshow-container">
-        {/* Display slideshow movies */}
-        {slideshowMovies.map((movie) => (
-          <div key={movie.id} className="slideshow-slide">
-            {/* Display movie details */}
-            <h2>{movie.title}</h2>
-            <p>{movie.overview}</p>
+    <div className="flex flex-col min-h-screen">
+      {/* Main content */}
+      <div className="flex-grow">
+        {/* Slideshow component */}
+        <div className="slideshow-container mb-8 h-96 w-full"> {/* Define height and width */}
+          <Slider {...settings}>
+            {slideshowImages.map((image, index) => (
+              <div key={index} className="slideshow-slide h-full w-full flex justify-center items-center">
+                <img 
+                  src={image} // Ensure the src attribute is correctly set
+                  alt={`Slide ${index + 1}`} 
+                  className="object-cover object-center w-full h-full rounded-2xl" // Adjust height and width
+                />
+              </div>
+            ))}
+          </Slider>
+        </div>
+        
+        {/* Movie card container */}
+        <div className="flex flex-col items-center p-4">
+          <input
+            type="text"
+            className="w-full max-w-md p-2 pl-2 mb-12 rounded-full border text-gray-700 border-gray-300 focus:outline-none focus:border-blue-500"
+            placeholder="Search movies..."
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+            {movies.slice(0, 9).map((movie, index) => (
+              <motion.div 
+                key={movie.imdbID}
+                initial={{ opacity: 0, y: 50 }} // Initial state: hidden and moved down
+                animate={{ opacity: 1, y: 0 }} // Animation when appearing
+                transition={{ duration: 0.5, delay: index * 0.1 }} // Transition animation duration with stagger effect
+                whileHover={{ scale: 1.1 }} // Zoom effect on hover
+                className="relative max-w-xs rounded-xl overflow-hidden shadow-lg bg-gradient-to-r from-gray-800 to-fuchsia-950 p-4"
+                onMouseEnter={() => handleMouseEnter(movie.imdbID)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <img className="w-full" src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/150'} alt={movie.Title} />
+                {selectedMovie && selectedMovie.imdbID === movie.imdbID && showDetails && (
+                  <motion.div 
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.7 }}
+                    className="absolute bottom-0 left-0 right-0 bg-gray-900 bg-opacity-85 text-white p-4"
+                  >
+                    <h2 className="text-lg font-bold">{selectedMovie.Title}</h2>
+                    <p>{selectedMovie.Plot}</p>
+                    <p>Released: {selectedMovie.Released}</p>
+                    <p>Runtime: {selectedMovie.Runtime}</p>
+                  </motion.div>
+                )}
+              </motion.div>
+            ))}
+            {additionalMovies.map((movie, index) => (
+              <motion.div 
+                key={movie.imdbID}
+                initial={{ opacity: 0, y: 50 }} // Initial state: hidden and moved down
+                animate={{ opacity: 1, y: 0 }} // Animation when appearing
+                transition={{ duration: 0.5, delay: index * 0.1 }} // Transition animation duration with stagger effect
+                whileHover={{ scale: 1.1 }} // Zoom effect on hover
+                className="relative max-w-xs rounded-xl overflow-hidden shadow-lg bg-gradient-to-r from-gray-800 to-fuchsia-950 p-4"
+                onMouseEnter={() => handleMouseEnter(movie.imdbID)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <img className="w-full" src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/150'} alt={movie.Title} />
+                {selectedMovie && selectedMovie.imdbID === movie.imdbID && showDetails && (
+                  <motion.div 
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.7 }}
+                    className="absolute bottom-0 left-0 right-0 bg-gray-900 bg-opacity-85 text-white p-4"
+                  >
+                    <h2 className="text-lg font-bold">{selectedMovie.Title}</h2>
+                    <p>{selectedMovie.Plot}</p>
+                    <p>Released: {selectedMovie.Released}</p>
+                    <p>Runtime: {selectedMovie.Runtime}</p>
+                  </motion.div>
+                )}
+              </motion.div>
+            ))}
           </div>
-        ))}
-      </div>
-      
-      {/* Movie card container */}
-      <div className="flex flex-col items-center p-4 -mt-8">
-        <input
-          type="text"
-          className="w-full max-w-md p-2 pl-2 mb-12 rounded-full border text-gray-700 border-gray-300 focus:outline-none focus:border-blue-500"
-          placeholder="Search movies..."
-          value={searchQuery}
-          onChange={handleSearchChange}
-        />
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {movies.slice(0, 9).map((movie, index) => (
-            <motion.div 
-              key={movie.imdbID}
-              initial={{ opacity: 0, y: 50 }} // Initial state: hidden and moved down
-              animate={{ opacity: 1, y: 0 }} // Animation when appearing
-              transition={{ duration: 0.5, delay: index * 0.1 }} // Transition animation duration with stagger effect
-              className="relative max-w-xs rounded-xl overflow-hidden shadow-lg bg-gradient-to-r from-gray-800 to-fuchsia-950 p-4"
-              onMouseEnter={() => handleMouseEnter(movie.imdbID)}
-              onMouseLeave={handleMouseLeave}
-            >
-              <img className="w-full" src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/150'} alt={movie.Title} />
-              {selectedMovie && selectedMovie.imdbID === movie.imdbID && showDetails && (
-                <motion.div 
-                  initial={{ opacity: 0, y: 50 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.7 }}
-                  className="absolute bottom-0 left-0 right-0 bg-gray-900 bg-opacity-85 text-white p-4"
-                >
-                  <h2 className="text-lg font-bold">{selectedMovie.Title}</h2>
-                  <p>{selectedMovie.Plot}</p>
-                  <p>Released: {selectedMovie.Released}</p>
-                  <p>Runtime: {selectedMovie.Runtime}</p>
-                </motion.div>
-              )}
-            </motion.div>
-          ))}
         </div>
       </div>
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 };
---
+++ moviehub/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-4 mt-24 h-18 mb-4 flex justify-center items-center">
+      <div className="text-center">
+        <p>&copy; 2024 MovieHub. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
